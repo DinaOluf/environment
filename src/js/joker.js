9e358d2005c3d0fbca907a8b30984403bea86203
Dina <dina.olufsen@hotmail.com>
2022-10-21 13:04:33 +0200

    fixed eslint error

--- src/js/joker.js
+++ src/js/joker.js
@@ -5,7 +5,13 @@ import joker from 'give-me-a-joke';
 // joker.getRandomDadJoke(console.log);
 
 export function dadJoke() {
-  return new Promise(function (resolve, reject) {
-    joker.getRandomDadJoke(resolve);
+    return new Promise(function (resolve, reject) {
+        try {
+          joker.getRandomDadJoke(function (joke) {
+            resolve(joke);
+          });
+        } catch (error) {
+          reject(error);
+        }
   });
 }
