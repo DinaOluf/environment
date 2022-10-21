38f8e7ea94eef9de63cb43ec58de2e84cde22f98
Dina <dina.olufsen@hotmail.com>
2022-10-21 12:21:39 +0200

    format single quotes

--- src/js/joker.js
+++ src/js/joker.js
@@ -1,12 +1,11 @@
-import joker from "give-me-a-joke";
+import joker from 'give-me-a-joke';
 
 // console.log(joker.getRandomJoke().body);
 
 // joker.getRandomDadJoke(console.log);
 
 export function dadJoke() {
-    return new Promise(function(resolve, reject) {
-      joker.getRandomDadJoke(resolve);
-    })
-  }
-
+  return new Promise(function (resolve, reject) {
+    joker.getRandomDadJoke(resolve);
+  });
+}
